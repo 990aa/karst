f0c1a63f27ffbbd80146a52423b1da8accec1bd3
990aa <099ahadabdul@gmail.com>
2025-10-02 23:25:42 +0530

    error fix

--- karst/Converters/BindingConverters.cs
+++ karst/Converters/BindingConverters.cs
@@ -1,21 +1,107 @@
+using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
 using SkiaSharp;
 
-namespace karst.Converters;
-
-public class BindingConverters
-    public BindingConverters() { }
+namespace karst.Converters
 {
-    public static readonly IValueConverter IsNotNull = new FuncConverter<object, bool>(
-        value => value != null, 
-        value => throw new NotImplementedException());
+    public class BindingConverters
+    {
+        public BindingConverters() { }
 
-    public static readonly IValueConverter IsEqual = new FuncConverter<object, object, object, bool>(
-        (value, parameter, culture) => Equals(value, parameter),
-        (value, parameter, culture) => throw new NotImplementedException());
+        // Instance properties for XAML
+        public IValueConverter IsNotNull => StaticIsNotNull;
+        public IValueConverter IsEqual => StaticIsEqual;
+        public IValueConverter SKColorToColor => StaticSKColorToColor;
 
-    public static readonly IValueConverter SKColorToColor = new FuncConverter<SKColor, Color>(
-        skColor => Color.FromRgba(skColor.Red, skColor.Green, skColor.Blue, skColor.Alpha),
-        color => throw new NotImplementedException());
+        // Static fields for code
+        public static readonly IValueConverter StaticIsNotNull = new FuncConverter<object, bool>(
+            value => value != null,
+            _ => throw new NotImplementedException());
+
+        public static readonly IValueConverter StaticIsEqual = new FuncConverter<object, object, object, bool>(
+            (value, parameter, culture) => Equals(value, parameter),
+            (value, parameter, culture) => throw new NotImplementedException());
+
+        public static readonly IValueConverter StaticSKColorToColor = new FuncConverter<SKColor, Color>(
+            skColor => Color.FromRgba(skColor.Red, skColor.Green, skColor.Blue, skColor.Alpha),
+            _ => throw new NotImplementedException());
+    }
+
+    public class FuncConverter<TFrom, TTo> : IValueConverter
+    {
+        private readonly Func<TFrom, TTo> _convert;
+        private readonly Func<TTo, TFrom>? _convertBack;
+
+        public FuncConverter(Func<TFrom, TTo> convert, Func<TTo, TFrom>? convertBack = null)
+        {
+            _convert = convert ?? throw new ArgumentNullException(nameof(convert));
+            _convertBack = convertBack;
+        }
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is null) return default;
+            return _convert((TFrom)value);
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (_convertBack != null && value is not null)
+                return _convertBack((TTo)value);
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FuncConverter<TFrom, TParam, TTo> : IValueConverter
+    {
+        private readonly Func<TFrom, TParam, TTo> _convert;
+        private readonly Func<TTo, TParam, TFrom>? _convertBack;
+
+        public FuncConverter(Func<TFrom, TParam, TTo> convert, Func<TTo, TParam, TFrom>? convertBack = null)
+        {
+            _convert = convert ?? throw new ArgumentNullException(nameof(convert));
+            _convertBack = convertBack;
+        }
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is null || parameter is null) return default;
+            return _convert((TFrom)value, (TParam)parameter);
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (_convertBack != null && value is not null && parameter is not null)
+                return _convertBack((TTo)value, (TParam)parameter);
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FuncConverter<TFrom, TParam, TCulture, TTo> : IValueConverter
+    {
+        private readonly Func<TFrom, TParam, TCulture, TTo> _convert;
+        private readonly Func<TTo, TParam, TCulture, TFrom>? _convertBack;
+
+        public FuncConverter(Func<TFrom, TParam, TCulture, TTo> convert, Func<TTo, TParam, TCulture, TFrom>? convertBack = null)
+        {
+            _convert = convert ?? throw new ArgumentNullException(nameof(convert));
+            _convertBack = convertBack;
+        }
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is null || parameter is null || culture is null) return default;
+            return _convert((TFrom)value, (TParam)parameter, (TCulture)(object)culture);
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (_convertBack != null && value is not null && parameter is not null && culture is not null)
+                return _convertBack((TTo)value, (TParam)parameter, (TCulture)(object)culture);
+            throw new NotImplementedException();
+        }
+    }
 }
 
 public class FuncConverter<TFrom, TTo> : IValueConverter
